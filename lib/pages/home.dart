3cb3a4db5fe4f98569ca719975753c15cd604cbe
İrem Deniz <iremd0550@gmail.com>
2024-06-19 21:29:15 +0300

    Updated home page.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,16 +9,101 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Breakfast',
-          style: GoogleFonts.lato(
-              textStyle: TextStyle(color: Colors.blue),
-              fontSize: 18,
-              fontWeight: FontWeight.bold),
-        ),
-        centerTitle: true,
+      appBar: appBar(), //extract method to have a clean code.
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xff1D1617).withOpacity(0.11),
+                  blurRadius: 40,
+                  spreadRadius: 0.0,
+                ),
+              ],
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  contentPadding: EdgeInsets.all(15), //to reduce the height.
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.all(13.0),
+                    child: SvgPicture.asset('assets/icons/search.svg'),
+                  ),
+                  suffixIcon: IntrinsicHeight(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        VerticalDivider(
+                          color: Colors.black,
+                          indent: 10,
+                          endIndent: 10,
+                          thickness: 0.1,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(11.0),
+                          child: SvgPicture.asset('assets/icons/filter.svg'),
+                        ),
+                      ],
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide.none)),
+            ),
+          )
+        ],
       ),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text(
+        'Breakfast',
+        style: GoogleFonts.lato(
+            textStyle: TextStyle(color: Colors.blue),
+            fontSize: 18,
+            fontWeight: FontWeight.bold),
+      ),
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: () {},
+        child: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          child: SvgPicture.asset(
+            'assets/icons/arrow.svg',
+            height: 20,
+            width: 20,
+          ),
+          decoration: BoxDecoration(
+              color: Color(0xffF7f8f8),
+              borderRadius: BorderRadius.circular(10)),
+        ),
+      ),
+      actions: [
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            margin: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            width: 37,
+            child: SvgPicture.asset(
+              'assets/icons/dots.svg',
+              height: 20,
+              width: 20,
+            ),
+            decoration: BoxDecoration(
+                color: Color(0xffF7f8f8),
+                borderRadius: BorderRadius.circular(10)),
+          ),
+        ),
+      ],
     );
   }
 }
